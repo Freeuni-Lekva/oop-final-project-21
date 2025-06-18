3bc2e09f795e53731ee802f2299e3c6524abbf36
amama22 <amama22@freeuni.edu.ge>
2025-06-18 04:46:09 +0400

    db+ContextListener setup

---
+++ quiz-website/src/main/java/ContextListener.java
@@ -0,0 +1,29 @@
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+
+    private DataSource dataSource;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        dataSource = DBUtil.createDataSource();
+        ServletContext context = sce.getServletContext();
+        context.setAttribute("dataSource", dataSource);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        try {
+            if (dataSource instanceof AutoCloseable) {
+                ((AutoCloseable) dataSource).close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ quiz-website/src/main/java/DBUtil.java
@@ -0,0 +1,19 @@
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+
+public class DBUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/db21?useSSL=false&serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Tasobaso123321.";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public static DataSource createDataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(URL);
+        ds.setUsername(USERNAME);
+        ds.setPassword(PASSWORD);
+        ds.setDriverClassName(DRIVER);
+        return ds;
+    }
+}
