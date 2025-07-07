6918a14ae2f0344cf0bfcdbbae11378ff73f1949
amama22 <amama22@freeuni.edu.ge>
2025-07-07 20:05:56 +0400

    resolved merge conflicts with develop

--- quiz-website/src/main/java/com/freeuni/quiz/util/DBUtil.java
+++ quiz-website/src/main/java/com/freeuni/quiz/util/DBUtil.java
@@ -7,9 +7,9 @@ import javax.sql.DataSource;
 public class DBUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/db21?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String USERNAME = "root";
-    //private static final String PASSWORD = "Tasobaso123321.";
+    private static final String PASSWORD = "Tasobaso123321.";
     //private static final String PASSWORD = "balibarcelona13";
-    private static final String PASSWORD = "";
+    //private static final String PASSWORD = "";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
 
