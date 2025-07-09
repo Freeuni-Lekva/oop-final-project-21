7aea5d582a8cdd0642e34450a5f8b228061d1a14
ramisha35 <trami21@freeuni.edu.ge>
2025-07-09 21:05:04 +0400

    Add QuizRating and QuizReview entity classes

---
+++ quiz-website/src/main/java/com/freeuni/quiz/bean/QuizReview.java
@@ -0,0 +1,88 @@
+package com.freeuni.quiz.bean;
+
+import java.sql.Timestamp;
+
+public class QuizReview {
+    private Long id;
+    private Integer userId;
+    private Long quizId;
+    private String reviewText;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+
+    private String userName;
+    private String userImageURL;
+
+    public QuizReview() {
+    }
+
+    public QuizReview(Integer userId, Long quizId, String reviewText) {
+        this.userId = userId;
+        this.quizId = quizId;
+        this.reviewText = reviewText;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Long getQuizId() {
+        return quizId;
+    }
+
+    public void setQuizId(Long quizId) {
+        this.quizId = quizId;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserImageURL() {
+        return userImageURL;
+    }
+
+    public void setUserImageURL(String userImageURL) {
+        this.userImageURL = userImageURL;
+    }
+}
